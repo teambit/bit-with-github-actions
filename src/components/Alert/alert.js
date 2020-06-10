5e18e5693a867bf873f09d51f7884dff35fadf4f
Josh Kuttler <yoshuakuttler@gmail.com>
2020-06-10 22:40:34 +0300

    update alert component

--- src/components/Alert/alert.js
+++ src/components/Alert/alert.js
@@ -4,9 +4,9 @@ import { getRandomColor } from '@bit/joshk.jotils.get-random-color'
 import { Label } from '@bit/bit.base-ui.elements.label'
 import styles from './alert.module.css'
 
-export const Alert = ({ text, color }) => {
+export const Alert = ({ text, color, ...rest }) => {
   return (
-    <Label color={color} className={styles.alert}>
+    <Label color={color} className={styles.alert} {...rest}>
       {text}
     </Label>
   )
